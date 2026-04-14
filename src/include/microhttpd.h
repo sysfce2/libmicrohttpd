4f049186bfe22ba12c07279f2eef99293798a710
Evgeny Grin (Karlson2k) <k2k@drgrin.dev>
2026-04-14 19:32:32 +0200

    Fixed missing MHD_VERSION bump

--- src/include/microhttpd.h
+++ src/include/microhttpd.h
@@ -101,7 +101,7 @@ MHD_C_DECLRATIONS_START_HERE_
  * they are parsed as decimal numbers.
  * Example: 0x01093001 = 1.9.30-1.
  */
-#define MHD_VERSION 0x01000300
+#define MHD_VERSION 0x01000400
 
 /* If generic headers don't work on your platform, include headers
    which define 'va_list', 'size_t', 'ssize_t', 'intptr_t', 'off_t',
